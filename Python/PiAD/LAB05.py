a6ec402512bb2b7b65eb4d5dcbc958a77e62c2b4
kacper <mk55552@zut.edu.pl>
2025-04-11 11:35:40 +0200

    huj

--- Python/PiAD/LAB05.py
+++ Python/PiAD/LAB05.py
@@ -1,16 +1,16 @@
-import numpy as np
-import scipy.sparse as sp
 import pandas as pd
+import numpy as np
+import scipy as sp
+from sklearn.datasets import fetch_rcv1
 
 
 def freq(x, prob=True):
     slownik = {}
-
     for y in x:
         if y not in slownik:
             slownik[y] = 1.0
         else:
-            slownik[y] += 1
+            slownik[y] += 1.0
 
     ni = list(slownik.values())
 
@@ -22,39 +22,56 @@ def freq(x, prob=True):
 
 def freq2(x, y, prob=True):
     slownik = {}
-
-    for x_values, y_values in zip(x, y):
-        if (x_values, y_values) not in slownik:
-            slownik[(x_values, y_values)] = 1.0
+    for x_val, y_val in zip(x, y):
+        key = (x_val, y_val)
+        if key not in slownik:
+            slownik[key] = 1.0
         else:
-            slownik[(x_values, y_values)] += 1.0
+            slownik[key] += 1.0
 
     ni = list(slownik.values())
 
     if prob:
         ni = [licz / len(x) for licz in ni]
 
-    return list(np.unique(x)), list(np.unique(y)), ni
+    x_unique = [k[0] for k in slownik.keys()]
+    y_unique = [k[1] for k in slownik.keys()]
+
+    return list(np.unique(x_unique)), list(np.unique(y_unique)), ni
 
 
 def entropy(x):
     _, pi = freq(x)
-    I_X = 0
-    for i in range(len(pi)):
-        I_X -= pi[i] * np.log2(pi[i])
-    return I_X
+    return -sum(p * np.log2(p) for p in pi if p > 0)
 
 
 def conditionalEntropy(x, y):
-    pass
+    xi, pi = freq(x)
+    cond_H = 0
+    for x_val, p_x in zip(xi, pi):
+        y_subset = [y[i] for i in range(len(x)) if x[i] == x_val]
+        cond_H += p_x * entropy(y_subset)
+    return cond_H
 
 
 def infoGain(x, y):
     return entropy(y) - conditionalEntropy(x, y)
 
 
-x = ['słońce', 'słońce', 'deszcz', 'deszcz', 'słońce']
-y = ['tak', 'tak', 'nie', 'nie', 'tak']
+zoo = pd.read_csv("csv/zoo.csv")
 
-ig = infoGain(x, y)
-print(f"Przyrost informacji: {ig:.4f}")
+X = zoo.iloc[:, :-1].values
+y = zoo.iloc[:, -1].values
+
+features = zoo.columns[:-1]
+target = zoo.columns[-1]
+
+for col in features:
+    ig = infoGain(zoo[col].tolist(), zoo[target].tolist())
+    print(f"{col}: IG = {ig:.4f}")
+
+
+# zadanie 6
+rcv1 = fetch_rcv1()
+X = rcv1["data"]
+Y = rcv1.target[:, 87]
