128d504cd6f2b8c1f3f9aea7402db9dedce4eca9
kacper <157168912+kacperMa25@users.noreply.github.com>
2025-05-29 20:30:15 +0200

    Delete C_C++/myLibs directory

--- C_C++/myLibs/someLibrary/someLib.cpp
+++
@@ -1,5 +0,0 @@
-#include "someLib.h"
-
-template <typename T> T kacper::add(T a, T b) { return a + b; }
-
-template <typename T> T kacper::mul(T a, T b) { return a * b; }
--- C_C++/myLibs/someLibrary/someLib.h
+++
@@ -1,5 +0,0 @@
-
-namespace kacper {
-template <typename T> T add(T a, T b);
-template <typename T> T mul(T a, T b);
-} // namespace kacper
